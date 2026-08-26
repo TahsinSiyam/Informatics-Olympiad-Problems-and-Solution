90c91da02eba607d70f02238dbffcb0a25ebbcc1
Tahsin Siyam <tahsinsiyam13@gmail.com>
2026-08-26 17:38:50 +0600

    Update First time the Main.CPP

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
-#include <iostream>
+#include <bits/stdc++.h>
+
 using namespace std;
 
-int main() {
-    cout << "Hello GitHub!\n";
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    return 0;
 }
